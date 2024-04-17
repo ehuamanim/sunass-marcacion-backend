63b4dff9ba9f88a373bae32894b90d16c6e6d7f3
Ericson Huamani <ehuamani.dev@gmail.com>
2024-04-16 23:55:37 -0500

    first commit

---
+++ src/main/java/pe/gob/sunass/marcacion/MarcacionApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.sunass.marcacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/gob/sunass/marcacion/MarcacionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.sunass.marcacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
